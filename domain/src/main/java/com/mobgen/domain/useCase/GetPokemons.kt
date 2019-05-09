303606ec114c501e1013bc5dfeca50a34c1d24d6
Ruiz Moncayo, Manuel <manuel.ruiz.moncayo@accenture.com>
2019-05-09 16:25:47 +0200

    bug fixed in pokedex 2

--- domain/src/main/java/com/mobgen/domain/useCase/GetPokemons.kt
+++ domain/src/main/java/com/mobgen/domain/useCase/GetPokemons.kt
@@ -1,8 +1,6 @@
 package com.mobgen.domain.useCase
 
 import com.mobgen.domain.PokemonRepository
-import com.mobgen.domain.Util
-import com.sun.org.apache.xalan.internal.utils.XMLSecurityManager
 import javax.inject.Inject
 
 class GetPokemons @Inject constructor(private val repository: PokemonRepository) {
@@ -11,9 +9,11 @@ class GetPokemons @Inject constructor(private val repository: PokemonRepository)
     }
 
     fun execute() =
-        repository.getPokemons().map { it.also { pokemons -> offset += pokemons.size } }
+        repository.getPokemons().map { it.also { pokemons -> offset = pokemons.size.toLong() } }
 
-    fun next() = repository.getNextPokemons(offset).map { it.also { pokemons ->
-        offset += pokemons.size
-    } }
+    fun next() = repository.getNextPokemons(offset).map {
+        it.also { pokemons ->
+            offset += pokemons.size
+        }
+    }
 }
